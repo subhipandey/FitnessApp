7572ba98ab89197aa88b5abaf54db8d0dd5a1c5a
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-24 14:38:52 +0530

    Created UI

--- app/src/main/java/com/subhipandey/android/workoutapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.subhipandey.android.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Here we will start the exercise",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
