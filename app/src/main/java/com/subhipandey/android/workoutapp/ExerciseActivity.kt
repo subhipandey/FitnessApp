3e11b211c3a2facc51466a4202ef4d98db8a90d2
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-27 22:13:57 +0530

    Added functionality to show current exercise + minor changes

--- app/src/main/java/com/subhipandey/android/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/ExerciseActivity.kt
@@ -1,6 +1,7 @@
 package com.subhipandey.android.workoutapp
 
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -21,11 +22,14 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
-    private var exerciseTimerDuration: Long = 30
-    private var currentExercisePosition = -1
+
     private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
+
     private var tts: TextToSpeech? = null
+
     private var player: MediaPlayer? = null
+
 
     private var exerciseAdapter: ExerciseStatusAdapter? = null
 
@@ -34,26 +38,23 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         setContentView(R.layout.activity_exercise)
 
         setSupportActionBar(toolbar_exercise_activity)
-        val actionbar = supportActionBar
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true)
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
 
-
         tts = TextToSpeech(this, this)
 
         exerciseList = Constants.defaultExerciseList()
+
         setupRestView()
 
+
         setupExerciseStatusRecyclerView()
-
-
     }
 
-    override fun onDestroy() {
+    public override fun onDestroy() {
         if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
@@ -63,76 +64,42 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             exerciseTimer!!.cancel()
             exerciseProgress = 0
         }
+
         if (tts != null) {
             tts!!.stop()
             tts!!.shutdown()
         }
+
         if (player != null) {
             player!!.stop()
         }
-
         super.onDestroy()
     }
 
-    private fun setRestProgressBar() {
-        progressBar.progress = restProgress
-        restTimer = object : CountDownTimer(10000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                restProgress++
-                progressBar.progress = 10 - restProgress
-                tvTimer.text = (10 - restProgress).toString()
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
             }
 
-            override fun onFinish() {
-                currentExercisePosition++
-                setupExerciseView()
-            }
-        }.start()
-    }
-
-    private fun setExerciseProgressBar() {
-        progressBarExercise.progress = exerciseProgress
-        exerciseTimer = object : CountDownTimer(exerciseTimerDuration * 1000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                exerciseProgress++
-                progressBarExercise.progress = exerciseTimerDuration.toInt() - restProgress
-                tvExerciseTimer.text = (exerciseTimerDuration.toInt() - restProgress).toString()
-            }
-
-
-            override fun onFinish() {
-                if (currentExercisePosition < exerciseList?.size!! - 1) {
-                    setupRestView()
-                } else {
-                    Toast.makeText(
-                        this@ExerciseActivity,
-                        "You have finished 7 minutes workout", Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }.start()
-    }
-
-    private fun setupExerciseView() {
-        llRestView.visibility = View.GONE
-        llRestView.visibility = View.VISIBLE
-        if (exerciseTimer != null) {
-            exerciseTimer!!.cancel()
-            exerciseProgress = 0
+        } else {
+            Log.e("TTS", "Initialization Failed!")
         }
-
-        speakOut(exerciseList!![currentExercisePosition].getName())
-        setExerciseProgressBar()
-        ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
-        tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
     }
+
 
     private fun setupRestView() {
+
+
         try {
             player = MediaPlayer.create(applicationContext, R.raw.press_start)
             player!!.isLooping = false
             player!!.start()
-
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -141,34 +108,113 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         llRestView.visibility = View.VISIBLE
         llExerciseView.visibility = View.GONE
 
+
         if (restTimer != null) {
             restTimer!!.cancel()
             restProgress = 0
         }
-        tvUpcomingExerciseName.text = exerciseList!![currentExercisePosition + 1].getName()
-        setRestProgressBar()
 
+
+        tvUpcomingExerciseName.text = exerciseList!![currentExercisePosition + 1].getName()
+
+
+        setRestProgressBar()
     }
 
-    override fun onInit(status: Int) {
-        if (status == TextToSpeech.SUCCESS) {
-            val result = tts!!.setLanguage(Locale.US)
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
-                Log.e("TTS", "The language specified is not supported")
-        } else {
-            Log.e("TTS", "Initialization Failed")
+    private fun setRestProgressBar() {
+
+        progressBar.progress = restProgress
+
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10 - restProgress
+                tvTimer.text =
+                    (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                currentExercisePosition++
+
+
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
+
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+
+    private fun setupExerciseView() {
+
+
+        llRestView.visibility = View.GONE
+        llExerciseView.visibility = View.VISIBLE
+
+
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
         }
+
+
+        ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
+
+        setExerciseProgressBar()
+    }
+
+
+    private fun setExerciseProgressBar() {
+
+        progressBarExercise.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBarExercise.progress = 30 - exerciseProgress
+                tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
+
+                if (currentExercisePosition < 11) {
+                    setupRestView()
+                } else {
+
+                    Toast.makeText(
+                        this@ExerciseActivity,
+                        "Congratulations! You have completed the 7 minutes workout.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }.start()
     }
 
     private fun speakOut(text: String) {
         tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
+
     private fun setupExerciseStatusRecyclerView() {
+
+
         rvExerciseStatus.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         exerciseAdapter = ExerciseStatusAdapter(exerciseList!!, this)
+
+
         rvExerciseStatus.adapter = exerciseAdapter
     }
-
 }
--- app/src/main/java/com/subhipandey/android/workoutapp/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/ExerciseStatusAdapter.kt
@@ -1,9 +1,11 @@
 package com.subhipandey.android.workoutapp
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_exercise_status.view.*
 
@@ -27,5 +29,23 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>, val context: Co
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model: ExerciseModel = items[position]
         holder.tvItem.text = model.getId().toString()
+
+        if (model.getIsSelected()) {
+            holder.tvItem.background =
+                ContextCompat.getDrawable(
+                    context,
+                    R.drawable.item_circular_thin_color_accent_border
+                )
+            holder.tvItem.setTextColor(Color.parseColor("#212121")) // Parse the color string, and return the corresponding color-int.
+        } else if (model.getIsCompleted()) {
+            holder.tvItem.background =
+                ContextCompat.getDrawable(context, R.drawable.item_circular_color_accent_background)
+            holder.tvItem.setTextColor(Color.parseColor("#FFFFFF"))
+        } else {
+            holder.tvItem.background =
+                ContextCompat.getDrawable(context, R.drawable.item_circular_color_grey_background)
+            holder.tvItem.setTextColor(Color.parseColor("#212121"))
+        }
+
     }
 }
