7ea50674fa06f0370f741ac0780feec3680ada56
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-28 18:08:13 +0530

    Setup SqliteOpenHelper to store date in db

--- app/src/main/java/com/subhipandey/android/workoutapp/FinishActivity.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/FinishActivity.kt
@@ -2,7 +2,10 @@ package com.subhipandey.android.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,17 +14,31 @@ class FinishActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar_finish_activity)
         val actionBar = supportActionBar
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
 
-        toolbar_finish_activity.setNavigationOnClickListener{
+        toolbar_finish_activity.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        btnFinish.setOnClickListener{
+        btnFinish.setOnClickListener {
             finish()
         }
-
+        addDateToDatabase()
     }
+
+    private fun addDateToDatabase() {
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("DATE", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        val dbHandler = SqliteOpenHelper(this, null)
+        dbHandler.addDate(date)
+        Log.i("DATE:", "Added")
+    }
+
+
 }
---
+++ app/src/main/java/com/subhipandey/android/workoutapp/SqliteOpenHelper.kt
@@ -0,0 +1,55 @@
+package com.subhipandey.android.workoutapp
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SqliteOpenHelper(
+    context: Context,
+    factory: SQLiteDatabase.CursorFactory?
+) :
+    SQLiteOpenHelper(
+        context, DATABASE_NAME,
+        factory, DATABASE_VERSION
+    ) {
+
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val CREATE_HISTORY_TABLE = ("CREATE TABLE " +
+                TABLE_HISTORY + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," +
+                COLUMN_COMPLETED_DATE
+                + " TEXT)")
+        db.execSQL(CREATE_HISTORY_TABLE)
+    }
+
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_HISTORY")
+        onCreate(db)
+    }
+
+
+    fun addDate(date: String) {
+        val values =
+            ContentValues()
+        values.put(
+            COLUMN_COMPLETED_DATE,
+            date
+        )
+        val db =
+            this.writableDatabase
+        db.insert(TABLE_HISTORY, null, values)
+        db.close()
+    }
+
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "Workout.db"
+        private const val TABLE_HISTORY = "history"
+        private const val COLUMN_ID = "_id"
+        private const val COLUMN_COMPLETED_DATE = "completed_date"
+    }
+}
