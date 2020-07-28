6803cb2ac49e16873dffe7bd3e8ed52a0be33817
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-28 12:26:16 +0530

    Added US Metrics

--- app/src/main/java/com/subhipandey/android/workoutapp/BMIActivity.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/BMIActivity.kt
@@ -3,6 +3,7 @@ package com.subhipandey.android.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_b_m_i.*
 import java.math.BigDecimal
@@ -10,22 +11,41 @@ import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
 
+
+    companion object {
+        private const val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW"
+        private const val US_UNITS_VIEW = "US_UNIT_VIEW"
+    }
+
+    private var currentVisibleView: String =
+        METRIC_UNITS_VIEW
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b_m_i)
 
         setSupportActionBar(toolbar_bmi_activity)
 
-        val actionbar = supportActionBar
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true)
-            actionbar.title = "CALCULATE BMI"
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "CALCULATE BMI"
 
         toolbar_bmi_activity.setNavigationOnClickListener {
             onBackPressed()
         }
 
+
+        makeVisibleMetricUnitsView()
+
+
+        rgUnits.setOnCheckedChangeListener { radioGroup: RadioGroup, checkedId: Int ->
+
+
+            if (checkedId == R.id.rbMetricUnits) {
+                makeVisibleMetricUnitsView()
+            } else {
+                makeVisibleUsUnitsView()
+            }
+        }
 
         btnCalculateUnits.setOnClickListener {
 
@@ -47,9 +67,37 @@ class BMIActivity : AppCompatActivity() {
                     .show()
             }
         }
-
     }
 
+
+    private fun makeVisibleMetricUnitsView() {
+        currentVisibleView = METRIC_UNITS_VIEW
+        llMetricUnitsView.visibility = View.VISIBLE
+        llUsUnitsView.visibility = View.GONE
+
+        etMetricUnitHeight.text!!.clear()
+        etMetricUnitWeight.text!!.clear()
+
+        tvYourBMI.visibility = View.INVISIBLE
+        tvBMIValue.visibility = View.INVISIBLE
+        tvBMIType.visibility = View.INVISIBLE
+        tvBMIDescription.visibility = View.INVISIBLE
+    }
+
+    private fun makeVisibleUsUnitsView() {
+        currentVisibleView = US_UNITS_VIEW
+        llMetricUnitsView.visibility = View.GONE
+        llUsUnitsView.visibility = View.VISIBLE
+
+        etUsUnitWeight.text!!.clear()
+        etUsUnitHeightFeet.text!!.clear()
+        etUsUnitHeightInch.text!!.clear()
+
+        tvYourBMI.visibility = View.INVISIBLE
+        tvBMIValue.visibility = View.INVISIBLE
+        tvBMIType.visibility = View.INVISIBLE
+        tvBMIDescription.visibility = View.INVISIBLE
+    }
 
     private fun validateMetricUnits(): Boolean {
         var isValid = true
@@ -63,23 +111,33 @@ class BMIActivity : AppCompatActivity() {
         return isValid
     }
 
+
     private fun displayBMIResult(bmi: Float) {
 
         val bmiLabel: String
         val bmiDescription: String
 
-        if (bmi.compareTo(15f) <= 0) {
+        if (java.lang.Float.compare(bmi, 15f) <= 0) {
             bmiLabel = "Very severely underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 15f) > 0 && java.lang.Float.compare(
+                bmi,
+                16f
+            ) <= 0
         ) {
             bmiLabel = "Severely underweight"
-            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 16f) > 0 && java.lang.Float.compare(
+                bmi,
+                18.5f
+            ) <= 0
         ) {
             bmiLabel = "Underweight"
-            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
-        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 18.5f) > 0 && java.lang.Float.compare(
+                bmi,
+                25f
+            ) <= 0
         ) {
             bmiLabel = "Normal"
             bmiDescription = "Congratulations! You are in a good shape!"
@@ -90,11 +148,17 @@ class BMIActivity : AppCompatActivity() {
         ) {
             bmiLabel = "Overweight"
             bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
-        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
+        } else if (java.lang.Float.compare(bmi, 30f) > 0 && java.lang.Float.compare(
+                bmi,
+                35f
+            ) <= 0
         ) {
             bmiLabel = "Obese Class | (Moderately obese)"
             bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
-        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0
+        } else if (java.lang.Float.compare(bmi, 35f) > 0 && java.lang.Float.compare(
+                bmi,
+                40f
+            ) <= 0
         ) {
             bmiLabel = "Obese Class || (Severely obese)"
             bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
@@ -115,5 +179,4 @@ class BMIActivity : AppCompatActivity() {
         tvBMIType.text = bmiLabel
         tvBMIDescription.text = bmiDescription
     }
-
 }
