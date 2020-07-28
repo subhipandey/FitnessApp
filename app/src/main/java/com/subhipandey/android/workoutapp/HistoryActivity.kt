3a5242fd32bce00f2b876d6bf73a5ea6a6a4a5ac
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-29 02:50:10 +0530

    Setup RecyclerView for history intent and final commit

--- app/src/main/java/com/subhipandey/android/workoutapp/HistoryActivity.kt
+++ app/src/main/java/com/subhipandey/android/workoutapp/HistoryActivity.kt
@@ -3,6 +3,8 @@ package com.subhipandey.android.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_history.*
 
 class HistoryActivity : AppCompatActivity() {
@@ -13,24 +15,45 @@ class HistoryActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar_history_activity)
 
-        val actionbar = supportActionBar
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true)
-            actionbar.title = "HISTORY"
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "HISTORY"
 
         toolbar_history_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
         getAllCompletedDates()
     }
 
-    private fun getAllCompletedDates() {
-        val dbHandler = SqliteOpenHelper(this, null)
-        val allCompletedDatesList = dbHandler.getAllCompleteDatesList()
 
-        for (i in allCompletedDatesList) {
-            Log.i("DateHISTORYACTIVITY", "" + i)
+    private fun getAllCompletedDates() {
+
+
+        val dbHandler = SqliteOpenHelper(this, null)
+
+        val allCompletedDatesList =
+            dbHandler.getAllCompleteDatesList()
+
+
+        if (allCompletedDatesList.size > 0) {
+
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+
+            rvHistory.layoutManager = LinearLayoutManager(this)
+
+
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+
+
+            rvHistory.adapter = historyAdapter
+        } else {
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
         }
+
     }
 }
